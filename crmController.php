28e4e9577068c643fd25a32367338c87e2d08661
eduardo1571 <2013030524@upsin.edu.mx>
2016-09-06 16:43:06 -0500

    Update crmController.php

--- crmController.php
+++ crmController.php
@@ -7,7 +7,9 @@ use Illuminate\Routing\Controller as BaseController;
 use Illuminate\Foundation\Validation\ValidatesRequests;
 use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
 
-class Controller extends BaseController
+class crmController extends Controller
 {
-    use AuthorizesRequests, DispatchesJobs, ValidatesRequests;
+    function welcome(){
+        
+    }
 }
